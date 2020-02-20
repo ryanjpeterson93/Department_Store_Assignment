ef254c16e5b3f45133eaa1e9b331eec4fd8da6e6
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-02-20 15:49:45 -0700

    added faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+5.times do
+  name = Faker::Commerce.department
+  department = Department.create(name: name)
+end
